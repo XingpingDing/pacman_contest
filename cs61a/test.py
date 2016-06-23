31342a7c7796788baa5d79e11f71f32fcef8a775
Xiu Yuliang <yuliangxiu@gmail.com>
2016-06-22 23:24:48 -0700

    hw1 of cs61a

--- cs61a/test.py
+++ cs61a/test.py
@@ -1,7 +1,11 @@
-x = 20
+import math
+def nearest_two(num):
+	ceil_num = math.ceil(math.log(num,2))
+	floor_num = math.floor(math.log(num,2))
+	if (pow(2,ceil_num)-num) > (num-pow(2,floor_num)):
+		power_of_two = pow(2,floor_num)
+	else:
+		power_of_two = pow(2,ceil_num)
+	return power_of_two
 
-def hello(x):
-	print('hello')
-	return x
 
-print(hello(5))
